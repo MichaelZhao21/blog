a662bd08e2a2d13b936679c4d164b5a7f38afcaa
Michael Zhao <michaelzhao314@gmail.com>
2024-03-01 04:09:18 -0600

    Added navbar

---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,65 @@
+<div class="navbar">
+	<h1 class="name">🐢 michael zhao</h1>
+	<div class="right">
+		<a href="/" class="navlink">home</a>
+		<a href="https://michaelzhao.xyz" class="navlink">about me</a>
+		<a href="/admin" class="navlink">admin</a>
+	</div>
+</div>
+
+<style>
+	.name {
+		flex-grow: 1;
+	}
+
+	.navbar {
+		padding-top: 1rem;
+		display: flex;
+		flex-direction: row;
+		justify-content: end;
+		align-items: center;
+		width: 100%;
+	}
+
+	.right {
+		display: flex;
+		flex-direction: row;
+		justify-content: end;
+		align-items: center;
+	}
+
+	.navlink {
+		margin: 0 1rem;
+		font-size: 1.25rem;
+		color: var(--primary);
+
+		text-decoration: underline;
+		text-decoration-color: transparent;
+		-webkit-text-decoration-color: transparent;
+		-moz-text-decoration-color: transparent;
+		transition: 200ms;
+	}
+
+	.navlink:hover {
+		text-decoration: underline;
+		text-decoration-color: var(--primary);
+		-webkit-text-decoration-color: var(--primary);
+		-moz-text-decoration-color: var(--primary);
+	}
+
+	/* Phone screen */
+	@media (max-width: 600px) {
+		.navbar {
+			flex-direction: column;
+			align-items: start;
+		}
+
+		.navlink {
+			margin: 0 0.5rem;
+		}
+
+		.navlink:first-child {
+			margin-left: 0;
+		}
+	}
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,69 @@
+<script>
+	import Footer from '$lib/Footer.svelte';
+	import Navbar from '$lib/Navbar.svelte';
+</script>
+
+<div class="container">
+	<Navbar />
+	<slot />
+	<Footer />
+</div>
+
+<style>
+	:global(:root) {
+		font-family: 'Open Sans', sans-serif;
+		line-height: 1.5;
+		font-weight: 400;
+
+		font-synthesis: none;
+		text-rendering: optimizeLegibility;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+	}
+
+	:global(h1),
+	:global(h2),
+	:global(h3),
+	:global(h4),
+	:global(h5),
+	:global(h6) {
+		font-family: 'Rubik', sans-serif;
+		padding: 0;
+		margin: 0;
+	}
+
+	:global(html),
+	:global(body) {
+		margin: 0;
+		background-color: var(--bg);
+	}
+
+	/* Light theme */
+	:global(body) {
+		--bg: #f2f0eb;
+		--text-light: #1b1f26;
+		--text: #000000;
+		--primary: #ae2eeb;
+	}
+
+	/* Dark theme */
+	:global(body.dark) {
+		--bg: #1b1f26;
+		--text-light: DDDDDD;
+		--text: #f2f0eb;
+		--primary: #da95fb;
+	}
+
+	.container {
+		width: calc(1200px + 2rem);
+		margin: 0 auto;
+	}
+
+    /* Container on ipad */
+    @media (max-width: 1200px) {
+        .container {
+            width: calc(100% - 2rem);
+            padding: 0 1rem;
+        }
+    }
+</style>
