3bc2661567731d0345214d342dedcffd527d2f3b
Michael Zhao <michaelzhao314@gmail.com>
2024-09-22 10:39:03 -0500

    Completed formatting for article page

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import Navbar from "../Navbar.svelte";
+	import Navbar from '../Navbar.svelte';
 
 	export let data;
 </script>
@@ -11,11 +11,15 @@
 <Navbar />
 
 <article>
-	<h1>{data.metadata.title}</h1>
-	<p>{data.metadata.date}&nbsp; · &nbsp;{data.readingTime} min read </p>
-	<div class="tags">
+	<h1 class="text-3xl md:text-5xl">{data.metadata.title}</h1>
+	<p class="font-bold text-grey-light mb-4">
+		{data.metadata.date}&nbsp; · &nbsp;{data.readingTime} min read
+	</p>
+	<div>
 		{#each data.metadata.tags.split(',') as tag}
-			<span class="tag">{tag.trim()}</span>
+			<span class="bg-background-light text-brown-1 rounded-md py-1 px-2 font-bold mr-4 text-sm">
+				{tag.trim()}
+			</span>
 		{/each}
 	</div>
 	<div class="content">
@@ -24,13 +28,71 @@
 </article>
 
 <style>
-	.tags {
+	.content > :global(h1) {
+		font-size: 2.25rem;
+		color: var(--brown);
+		margin-top: 2.5rem;
 		margin-bottom: 1rem;
+		line-height: 90%;
 	}
-	.tag {
-		background-color: #eee;
-		padding: 0.2rem 0.5rem;
-		margin-right: 0.5rem;
-		border-radius: 0.25rem;
+
+	.content > :global(h2) {
+		font-size: 1.75rem;
+		color: var(--brown-1);
+		margin-top: 1.75rem;
+		margin-bottom: 0.5rem;
+	}
+
+	.content > :global(h3) {
+		font-size: 1.5rem;
+		color: var(--brown-2);
+		margin-top: 1.25rem;
+		margin-bottom: 0.25rem;
+	}
+
+	.content > :global(p) {
+		font-size: 1rem;
+		color: var(--text);
+		margin-bottom: 0.75rem;
+	}
+
+	.content > :global(ul) {
+		list-style: inside !important;
+		margin-left: 0.5rem;
+		margin-bottom: 0.75rem;
+	}
+
+
+	.content > :global(ol) {
+		list-style: decimal inside !important;
+		margin-left: 0.5rem;
+		margin-bottom: 0.75rem;
+	}
+
+	.content > :global(li) {
+		font-size: 1rem;
+	}
+
+	@media (min-width: 768px) {
+		.content > :global(h1) {
+			font-size: 2.5rem;
+		}
+
+		.content > :global(h2) {
+			font-size: 2rem;
+		}
+
+		.content > :global(h3) {
+			font-size: 1.75rem;
+		}
+
+		.content > :global(p) {
+			font-size: 1.15rem;
+		}
+
+		.content > :global(ol),
+		.content > :global(ul) {
+			font-size: 1.15rem;
+		}
 	}
 </style>
