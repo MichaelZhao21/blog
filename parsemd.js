7555e4fa3853589f6d5950d4ecaaf3dd5b853867
Michael Zhao <michaelzhao314@gmail.com>
2024-09-25 11:41:27 -0500

    Added sort by date for posts

--- parsemd.js
+++ parsemd.js
@@ -1,5 +1,9 @@
+import dayjs from 'dayjs';
+import customParseFormat from 'dayjs/plugin/customParseFormat.js';
 import fs from 'fs';
 import showdown from 'showdown';
+
+dayjs.extend(customParseFormat);
 
 // Script to parse the markdown in `posts` directory, convert them to svelte components, and put them in gen-posts folder in `src`
 // We will also generate a posts.json file that contains the metadata of the posts
@@ -32,6 +36,16 @@ async function main() {
 		}),
 	);
 
+	// Make sure there are no duplicates
+	checkDuplicateSlugs(postMetadata);
+
+	// Sort posts.json by date (dates are in format September 25th, 2024)
+	postMetadata.sort((a, b) => {
+		const dateA = dayjs(a.date, 'MMMM Do, YYYY');
+		const dateB = dayjs(b.date, 'MMMM Do, YYYY');
+		return dateB.diff(dateA);
+	});
+
 	// Write the metadata to posts.json
 	fs.writeFileSync('src/gen-posts/posts.json', JSON.stringify(postMetadata));
 }
@@ -39,13 +53,13 @@ async function main() {
 main();
 
 // Calculate the amount of time it takes to read, in minutes
-export function computeReadingTime(input) {
+function computeReadingTime(input) {
 	const wc = calcWords(input);
 	return Math.round(wc / 200);
 }
 
 // Calculate the number of words in a string, stripping non alphanumeric characters
-export function calcWords(input) {
+function calcWords(input) {
 	// Replace all non-alphanumeric characters
 	const replaced = input.replaceAll(/[^A-Za-z0-9 ]/g, '');
 	const split = replaced
@@ -53,4 +67,15 @@ export function calcWords(input) {
 		.map((s) => s.trim())
 		.filter((s) => s !== '');
 	return split.length;
+}
+
+// Check if any slugs are duplicated in the post metadata array
+function checkDuplicateSlugs(posts) {
+	const slugs = new Map();
+	for (const post of posts) {
+		if (slugs.has(post.slug)) {
+			throw new Error(`Duplicate slug: ${post.slug}. Posts: ${slugs.get(post.slug)}, ${post.title}`);
+		}
+		slugs.set(post.slug, post.title);
+	}
 }
