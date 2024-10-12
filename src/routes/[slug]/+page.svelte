91e0b542fe9c7fc079cf19ee3223755057ac1361
Michael Zhao <michaelzhao314@gmail.com>
2024-10-11 22:58:28 -0500

    Added disquis to blog

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -22,6 +22,21 @@
 	// Highlight all code after page loads
 	onMount(() => {
 		hljs.highlightAll();
+
+		// DISQUIS CONFIG
+		var disqus_config = function (this: any) {
+			this.page.url = `https://blog.michaelzhao.xyz/${slug}`; // Replace PAGE_URL with your page's canonical URL variable
+			this.page.identifier = slug; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+		};
+
+		(function () {
+			// DON'T EDIT BELOW THIS LINE
+			var d = document,
+				s = d.createElement('script');
+			s.src = 'https://mikey-blog.disqus.com/embed.js';
+			s.setAttribute('data-timestamp', (+new Date()).toString());
+			(d.head || d.body).appendChild(s);
+		})();
 	});
 </script>
 
@@ -56,9 +71,15 @@
 			</span>
 		{/each}
 	</div>
-	<div class="content">
+	<div class="content mb-16">
 		{@html data.content}
 	</div>
+	<div id="disqus_thread"></div>
+	<noscript>
+		Please enable JavaScript to view the
+		<a href="https://disqus.com/?ref_noscript">comments powered by Disqus. </a>
+	</noscript>
+	<script id="dsq-count-scr" src="//mikey-blog.disqus.com/count.js" async></script>
 </article>
 
 <button
